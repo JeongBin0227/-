853d2e2808b31687623fea612f0a2e0cccd5575a
KimJeongBin <kjh3095000@gmail.com>
2018-12-07 23:51:27 +0900

    A-B 문제에 관한 정답
    나의 풀이

---
+++ 1. 입출력/Subtraction.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int a;
+		int b;
+
+		a = (int) in.nextInt();
+		b = (int) in.nextInt();
+
+		System.out.println(a - b);
+
+	}
+
+}
