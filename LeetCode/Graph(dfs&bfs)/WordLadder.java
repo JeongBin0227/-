670e7f10ff0c8967ab45696ebad8a151ba868ca4
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-04-30 23:53:54 +0900

    [EDIT] WordLadder 수정

--- LeetCode/Graph(dfs&bfs)/WordLadder.java
+++ LeetCode/Graph(dfs&bfs)/WordLadder.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class WordLadder {
     public static void main(String[] args) {
-        String[] words = {"hot", "dot", "dog",  "cog","log"};
+        String[] words = {"hot","dot","dog","lot","log","cog"};
 //		String[] words = {"hot","dot","dog","lot","log"};
         List<String> wordList = Arrays.asList(words);
 //		Set<String> dict = new HashSet<>(wordList);
@@ -13,12 +13,20 @@ public class WordLadder {
     public int ladderLength_neighbor(String beginWord, String endWord, List<String> wordList) {
         if (!wordList.contains(endWord) ) return 0;
         boolean[] wordListCheck = new boolean[wordList.size()];
-        dfs(0,0,beginWord,endWord,wordListCheck,wordList);
+        dfs(1,0,beginWord,endWord,wordListCheck,wordList);
 
         return answer;
     }
     void dfs(int count, int time, String word, String endWord, boolean[] wordListCheck, List<String> wordList){
-        System.out.println("word"+word);
+        System.out.println("---------");
+        System.out.print("count  "+count);
+        System.out.print("word  "+word);
+        System.out.print("endWord  "+endWord);
+
+        if (word == endWord) {
+            System.out.println("count"+count);
+            if(answer>count)  answer = count;
+        }
         for (int i = 0; i < wordList.size(); i++) {
             int tmp = 0;
             if(wordListCheck[i]==false) {
@@ -29,12 +37,12 @@ public class WordLadder {
                 }
                 if (tmp == word.length() - 1) {
                     count++;
-                    System.out.println("word"+word);
-                    System.out.println("word"+endWord);
-                    if (word == endWord) {
-                        if(answer>count)  answer = count;
-                    }
                     wordListCheck[i] = true;
+                    System.out.println("");
+                    System.out.println("xxxxxxxxxxxxxxxx");
+                    System.out.print("count  "+count);
+                    System.out.print("word  "+word);
+                    System.out.print("endWord  "+endWord);
                     dfs(count, i, wordList.get(i),endWord, wordListCheck, wordList);
                 }
             }
